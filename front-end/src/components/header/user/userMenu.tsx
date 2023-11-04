7a699556815d8b3db1d7a082bdb59137d2875a18
DHughes666 <graziemolto6@gmail.com>
2023-11-04 10:55:57 +0100

    Profile link to blogs --> <-- established

--- front-end/src/components/header/user/userMenu.tsx
+++ front-end/src/components/header/user/userMenu.tsx
@@ -1,7 +1,45 @@
-
+import {useState} from 'react'
+import { Box, IconButton, Menu, MenuItem,
+Typography } from "@mui/material";
+import { FaUserNurse } from "react-icons/fa";
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { authActions } from '../../../store/auth-slice';
 
 const UserMenu = () => {
-    return <div>Users</div>
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const [anchorEl, setAnchorEl] = useState<Element | null>(null);
+
+    const handleLogout = () => {
+        dispatch(authActions.logout());
+        navigate("/")
+    }
+    const handleProfileClick = () => {
+        navigate("/profile")
+    }
+    return (
+        <Box>
+            <IconButton 
+                onClick={(e) => setAnchorEl(e.currentTarget)}  
+                color="inherit"
+            >
+                <FaUserNurse />
+            </IconButton>
+            <Menu 
+                anchorEl={anchorEl} 
+                open={Boolean(anchorEl)}
+                onClose={() => setAnchorEl(null)}
+            >
+                <MenuItem onClick={handleProfileClick}>
+                    <Typography>Profile</Typography>
+                </MenuItem>
+                <MenuItem onClick={handleLogout}>
+                    <Typography>Logout</Typography>
+                </MenuItem>
+            </Menu>
+        </Box>
+    )
 }
 
 export default UserMenu;

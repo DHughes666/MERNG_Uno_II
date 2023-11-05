bf4679f3c5020ef25528d3fc15bdec0c745c6e34
DHughes666 <graziemolto6@gmail.com>
2023-11-05 07:13:07 +0100

    Profile design ... In progress

--- front-end/src/components/header/user/profile.tsx
+++ front-end/src/components/header/user/profile.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+//@ts-nocheck
+import { Box, Typography, Avatar } from "@mui/material";
+import { profileStyles } from "./profile-styles";
+import { BlogItem } from "../../blogs/blogItem";
+import {RxAvatar} from 'react-icons/rx'
 
 const Profile = () => {
   return (
-    <div>profile</div>
+    <Box sx={profileStyles.container}>
+        <Box sx={profileStyles.blogsContainer}>
+            <Typography variant="h3" sx={profileStyles.text}>
+                My Posts
+            </Typography>
+            <Box sx={profileStyles.cardsContainer}>
+                {[1, 2, 3, 4, 5].map((item) => (
+                    <BlogItem 
+                    blog={{
+                        title: item.toString(),
+                        content: item.toString(),
+                        date: new Date(),
+                        id: item.toString(),
+                    }}
+                    />
+                ))}
+            </Box>
+        </Box>
+        <Box sx={profileStyles.profileContainer}>
+            <Box sx={profileStyles.userContainer}>
+                <Avatar sx={profileStyles.avatar}></Avatar>
+            </Box>
+        </Box>
+    </Box>
   )
 }
 
